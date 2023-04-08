d3e0949b9f7bc88da9e65404908dd826abb3e29a
Marxouo <78555292+marxeugenio@users.noreply.github.com>
2023-04-08 16:26:33 -0300

    Add files via upload
    Pergunte ao usuário se ele está com fome e tem dinheiro
    Apenas nos casos em que o usuário esteja com fome e possua dinheiro, indique um local que venda comida
    Mostre na tela uma mensagem, de sua escolha, quando ele não estiver com fome ou não possuir dinheiro

---
+++ fomeEdinheiro.py
@@ -0,0 +1,8 @@
+usuário = str(input('Você está com fome? '))
+dinheiro = str(input('Você tem dinheiro? '))
+
+if usuário == 'sim' and dinheiro == 'sim':
+    print('Vá a tenda da Dona Maricreusa, ela vende uma comida muito boa! ')
+
+else:
+    print('Infelizmente por você ou só ter dinheiro e não possuir fome, ou possuir fome e não ter dinheiro, vá embora pfv ): ')
