7c54b8ec0fea108f2d7d29322366445b10c47e8a
Marxouo <78555292+marxeugenio@users.noreply.github.com>
2023-05-11 11:06:49 -0300

    Add files via upload
    Este programa utiliza a biblioteca NumPy para realizar a multiplicação de duas matrizes.

---
+++ Multiplicacao_De_Matrizes.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# cria duas matrizes
+matriz1 = np.array([[1,2],[3,4]])
+matriz2 = np.array([[5,6],[7,8]])
+
+# multiplicar as matrizes
+resultado = np.dot(matriz1, matriz2)
+
+print(resultado)
