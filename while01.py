5749aea81bacf72f701e9f5cadc0c04c645d88e4
Marxouo <78555292+marxeugenio@users.noreply.github.com>
2023-05-19 10:52:07 -0300

    Add files via upload
    Validação de entrada de idade até que seja fornecida uma idade válida

--- while01.py
+++ while01.py
@@ -1,5 +1,6 @@
-i = 4
-
-while i < 8:
-    print(i)
-    i += 1
+age = -1
+while age < 0 or age > 120:
+    age = int(input("Digite sua idade: "))
+    if age < 0 or age > 120:
+        print("Idade invalida. Tente novamente")
+print("Idade valida")
